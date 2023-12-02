858eb913f9de90fe49f6de7dde993176eae897c5
om205 <om030402@gmail.com>
2023-12-02 17:54:50 +0530

    redesigned the ui

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const $modelImage = document.getElementById('model-image')
 const $modelDescription = document.getElementById('model-description')
 const $modelCodeBtn = document.getElementById('model-code-btn')
 const $modelLiveBtn = document.getElementById('model-live-btn')
+const $downloadResumeBtn = document.getElementById('resume-download-btn')
+const $resumeFrame = document.getElementById('resume-frame')
+const $address = document.getElementById('address')
 
-let projects
+let projects, resumeLink
 
 function togglePane() {
     if(getComputedStyle($toggler).display === 'none') return
@@ -57,6 +60,18 @@ $projects.addEventListener('click', e => {
     showProjectDetails(elementID)
 })
 
+$address.addEventListener('click', e => {
+    // click to copy
+    const text = e.target.innerText
+    navigator.clipboard.writeText(text)
+    alert('Address Copied')
+})
+
+$downloadResumeBtn.addEventListener('click', e => {
+    e.preventDefault()
+    window.open(resumeLink)
+})
+
 $toggler.children[0].addEventListener('click', togglePane)
 
 const fetchProjects = async () => {
@@ -66,3 +81,42 @@ const fetchProjects = async () => {
     setupProjects()
 }
 fetchProjects()
+
+const fetchResume = async () => {
+    const res = await fetch('https://docs.google.com/document/d/1p5MCbIWK-esIlpyDgy85uvXKkoYvoyGfTCcdVTPvpNs/export?format=txt')
+    const txt = await res.text()
+    resumeLink = JSON.parse(txt).resume
+    $resumeFrame.setAttribute("src",resumeLink)
+}
+fetchResume()
+
+function typeWriter(text, index, interval) {
+    if (index < text.length) {
+        document.getElementById('typing-container').innerHTML += text.charAt(index);
+        index++;
+        setTimeout(function () {
+            typeWriter(text, index, interval);
+        }, interval);
+    }
+}
+
+function startTypingAnimation() {
+    let container = document.getElementById('typing-container');
+    let text = ['Competitive Programmer.', 'exploring new opportunities.', 'Full Stack Web Developer.'];
+    let i = 2;    
+    container.innerHTML = '';
+    typeWriter(text[i], 0, 150);
+    i = 0;
+
+    setInterval(function () {
+        container.innerHTML = '';
+        typeWriter(text[i], 0, 150);
+        i++;
+        if (i >= text.length) i = 0;
+    }, 5000);
+}
+startTypingAnimation();
+
+
+console.log("%cHey there, Welcome to console! 👋", "font-size: 1.25rem; color: #fff; background-color: #7A1DA1; padding: 0.5rem; border-radius: 0.5rem;")
+console.log("%cTip: If some links and images do not work then try opening the website in incognito mode!", "font-size: 1.25rem; color: #eee; background-color: #7A1DA1; padding: 0.5rem; border-radius: 0.5rem;")
